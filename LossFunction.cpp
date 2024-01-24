716f9d49a13216b9454a5cc49edcd5f6194f7649
alinashipova <shipovaas1@gmail.com>
2024-01-24 17:20:00 +0300

    Init

--- LossFunction.cpp
+++ LossFunction.cpp
@@ -4,9 +4,6 @@
 LossFunction::LossFunction(const Vector &pred, Vector &result) : prediction(pred),real_result(result) {}
 
 double LossFunction::Distance() const {
-    double distance = 0.0;
-    for (size_t i = 0; i < prediction.size(); ++i) {
-        distance += pow(prediction[i] - real_result[i], 2);
-    };
+    double euclideanDistance = (prediction - real_result).norm();
     return sqrt(distance);
 }
