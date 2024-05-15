f41c8f44d38463c30eac52ef61bf3d4810df4f52
alinashipova <shipovaas1@gmail.com>
2024-05-15 21:36:32 +0300

    Init

--- src/Layer.cpp
+++ src/Layer.cpp
@@ -2,21 +2,26 @@
 #include <iostream>
 
 namespace neuralnet {
+    using Matrix = Eigen::MatrixXd;
+    using Vector = Eigen::VectorXd;
 
     Layer::Layer(int input_size, int output_size, ActivationFunction activation_func)
-            : weights(Eigen::MatrixXd::Random(output_size, input_size)),
-              biases(Eigen::VectorXd::Zero(output_size)),
+            : weights(Matrix::Random(output_size, input_size)),
+              biases(Vector::Random(output_size)),
               activation_function(std::move(activation_func)) {}
 
-    Eigen::VectorXd Layer::forward(const Eigen::VectorXd& input) {
+    Eigen::VectorXd Layer::forward(const Vector& input) {
         std::cout << "Input to layer size: " << input.size() << std::endl;
         input_cache = input;
-        Eigen::VectorXd z = weights * input + biases;
-        output_cache = z.unaryExpr([this](double val) { return activation_function.calc(val); });
-        return output_cache;
+        output_cache = weights * input + biases;
+        return activation_function.calc(output_cache);
+        //Vector z = weights * input + biases;
+        //output_cache = activation_function.calc(z);
+        //output_cache = z.unaryExpr([this](double val) { return activation_function.calc(val); });
+        //eturn output_cache;
     }
 
-    Eigen::VectorXd Layer::backward(const Eigen::VectorXd& grad_output, double learning_rate) {
+    Vector Layer::backward(const Vector& grad_output, double learning_rate) {
         std::cout << "Entering backward: grad_output size " << grad_output.size() << std::endl;
         std::cout << "weights.transpose() size: " << weights.transpose().rows() << "x" << weights.transpose().cols() << std::endl;
         std::cout << "output_cache size: " << output_cache.size() << std::endl;
@@ -24,16 +29,16 @@ namespace neuralnet {
         if (grad_output.size() != output_cache.size()) {
             throw std::runtime_error("Mismatch in grad_output size and output_cache size in backward propagation.");
         }
-        Eigen::VectorXd derivative = output_cache.unaryExpr([this](double val) { return activation_function.derivative(val); });
-        Eigen::VectorXd local_grad = grad_output.cwiseProduct(derivative);
-        Eigen::MatrixXd transposed_grad_weights = input_cache * local_grad.transpose();
-        Eigen::MatrixXd grad_weights = transposed_grad_weights.transpose();
+        Vector derivative = output_cache.unaryExpr([this](double val) { return activation_function.derivative(val); });
+        Vector local_grad = grad_output.cwiseProduct(derivative);
+        Matrix transposed_grad_weights = input_cache * local_grad.transpose();
+        Matrix grad_weights = transposed_grad_weights.transpose();
         std::cout<<" grad_weights.cols() "<<grad_weights.cols()<<std::endl;
         std::cout<<" weights.cols() "<<weights.cols()<<std::endl;
         std::cout<<" grad_weights.rows() "<<grad_weights.rows()<<std::endl;
         std::cout<<"weights.rows() "<< weights.rows()<<std::endl;
 
-        Eigen::VectorXd grad_input = weights.transpose() * local_grad;
+        Vector grad_input = weights.transpose() * local_grad;
         if (grad_input.size() != input_cache.size()) {
             throw std::runtime_error("Mismatch in input_cache size and grad_input size.");
         }
@@ -42,7 +47,7 @@ namespace neuralnet {
             throw std::runtime_error("Mismatch in dimensions for weight gradients.");
         }
 
-        Eigen::VectorXd grad_biases = local_grad;
+        Vector grad_biases = local_grad;
         if (grad_biases.size() != biases.size()) {
             throw std::runtime_error("Mismatch in biases size and grad_biases size.");
         }
@@ -63,11 +68,11 @@ namespace neuralnet {
         file.read(reinterpret_cast<char*>(biases.data()), biases.size() * sizeof(double));
     }
 
-    const Eigen::MatrixXd& Layer::get_weights() const {
+    const Matrix& Layer::get_weights() const {
         return weights;
     }
 
-    const Eigen::VectorXd& Layer::get_biases() const {
+    const Vector& Layer::get_biases() const {
         return biases;
     }
 }
